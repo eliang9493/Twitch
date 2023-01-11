02d9b08c107d522c9f21eb2078530eb6f9d2bea4
Emma Liang <97870806+eliang9493@users.noreply.github.com>
2023-01-10 23:04:50 -0600

    backend

---
+++ backend/service/LoginService.java
@@ -0,0 +1,17 @@
+package com.laioffer.jupiter.service;
+
+import com.laioffer.jupiter.dao.LoginDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private LoginDao loginDao;
+
+    public String verifyLogin(String userId, String password) throws IOException {
+        return loginDao.verifyLogin(userId, password);
+    }
+}
